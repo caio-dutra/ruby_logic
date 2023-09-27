62f4306aaac2ecde358cb976aa9c55003ff452ed
Caio Dutra <caiodutrasnt@gmail.com>
2023-09-27 16:54:37 -0300

    advanced_ruby_I_exercises

---
+++ _0challenges/capitalize.rb
@@ -0,0 +1,14 @@
+capit = -> (name){puts "#{name.capitalize()}"}
+
+def capitalize_name(capit)
+    print "Digite o primeiro nome: "
+    first_name = gets.chomp
+    print "Digite o segundo nome: "
+    second_name = gets.chomp
+
+    puts "\n"
+    capit.call(first_name)
+    capit.call(second_name)
+end
+
+capitalize_name(capit)
--- _6blocks_lambdas_modules/lambda.rb
+++ _6blocks_lambdas_modules/lambda.rb
@@ -1,9 +1,9 @@
-first_lambda = lambda { puts "my first lambda"}
-first_lambda.call
+# first_lambda = lambda { puts "my first lambda"}
+# first_lambda.call
 
-#other declaration for one line
-first_lambda = -> { puts "my first lambda"}
-first_lambda.call
+# #other declaration for one line
+# first_lambda = -> { puts "my first lambda"}
+# first_lambda.call
 
 #receiving parameters
 first_lambda = -> (names){ names.each { |name |puts name} }
@@ -13,28 +13,28 @@ names = ["joão", "maria", "pedro"]
 first_lambda.call(names)
 
 #lambda with more lines
-my_lambda = lambda do |numbers|
-    index = 0
-    puts 'Número atual + Próximo número'
-    numbers.each do |number|
-      return if numbers[index] == numbers.last
-      puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
-      puts numbers[index] + numbers[index + 1]
-      index += 1
-    end
-end
+# my_lambda = lambda do |numbers|
+#     index = 0
+#     puts 'Número atual + Próximo número'
+#     numbers.each do |number|
+#       return if numbers[index] == numbers.last
+#       puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
+#       puts numbers[index] + numbers[index + 1]
+#       index += 1
+#     end
+# end
    
-numbers = [1, 2, 3, 4]
+# numbers = [1, 2, 3, 4]
    
-my_lambda.call(numbers)
+# my_lambda.call(numbers)
 
-#passing multiple lambdas as arguments
-def foo(first_lambda, second_lambda)
-    first_lambda.call
-    second_lambda.call
-end
+# #passing multiple lambdas as arguments
+# def foo(first_lambda, second_lambda)
+#     first_lambda.call
+#     second_lambda.call
+# end
    
-first_lambda = lambda { puts "my first lambda"}
-second_lambda = lambda { puts "my second lambda"}
+# first_lambda = lambda { puts "my first lambda"}
+# second_lambda = lambda { puts "my second lambda"}
    
-foo(first_lambda, second_lambda)
+# foo(first_lambda, second_lambda)
